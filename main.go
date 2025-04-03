145876827e52ce604e37fe4cfcd751401a3d9b05
Mag <hicugi@gmail.com>
2025-04-03 15:42:12 +0500

    feat: listen tcp instead of reading from file

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"os"
+	"net"
 	"log"
 	"io"
 	"strings"
@@ -9,7 +9,8 @@ import (
 	"fmt"
 )
 
-const filePath = "./messages.txt"
+const CONNECTION_TYPE = "tcp"
+const PORT = 42069
 
 func getLinesChannel(f io.ReadCloser) <-chan string {
 	lines := make(chan string)
@@ -55,16 +56,27 @@ func getLinesChannel(f io.ReadCloser) <-chan string {
 }
 
 func main() {
-	file, err := os.Open(filePath)
-
+	listener, err := net.Listen(CONNECTION_TYPE, fmt.Sprintf("%s%d", ":", PORT))
 	if err != nil {
-		log.Fatalf("Coun't open %s: %s\n", filePath, err)
+		log.Fatalf("Coun't create listener '%s' by port :%d\n", CONNECTION_TYPE, PORT)
 		return
 	}
 
-	linesChan := getLinesChannel(file)
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatalf("- connection error: %s\n", err.Error())
+		}
 
-	for line := range linesChan {
-		fmt.Println("read:", line)
+		// addr := conn.RemoteAddr()
+		// fmt.Println("- connection is live from", addr)
+
+		linesChan := getLinesChannel(conn)
+
+		for line := range linesChan {
+			fmt.Println("read:", line)
+		}
+
+		// fmt.Println("- connect to", addr, "closed")
 	}
 }
