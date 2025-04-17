a2dd21fab68ceb8861b3de54143132f0712ac601
Mag <hicugi@gmail.com>
2025-04-17 17:20:58 +0500

    feat: parse headers

---
+++ internal/headers/headers.go
@@ -0,0 +1,69 @@
+package headers
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+)
+
+const CRLF = "\r\n"
+const CHARS = "!#$%&'*+-.^_`|~"
+
+type Headers map[string]string
+
+func NewHeaders() Headers {
+	return map[string]string{}
+}
+
+func (h Headers) Parse(data []byte) (n int, done bool, err error) {
+	idx := bytes.Index(data, []byte(CRLF))
+	if idx == -1 {
+		return 0, false, nil
+	}
+
+	if idx == 0 {
+		// headers are done, consume the CRLF
+		return 2, true, nil
+	}
+
+	parts := bytes.SplitN(data[:idx], []byte(":"), 2)
+	key := string(parts[0])
+
+	if key != strings.TrimRight(key, " ") {
+		return 0, false, fmt.Errorf("invalid header name: %s", key)
+	}
+
+	key = strings.ToLower(strings.TrimSpace(key))
+
+	if !validTokens([]byte(key)) {
+		return 0, false, fmt.Errorf("invalid header token found: %s", key)
+	}
+
+	value := strings.TrimSpace(string(parts[1]))
+	h[key] = string(value)
+
+	return idx + 2, false, nil
+}
+
+var tokenChars = []byte{'!', '#', '$', '%', '&', '\'', '*', '+', '-', '.', '^', '_', '`', '|', '~'}
+
+// validTokens checks if the data contains only valid tokens
+// or characters that are allowed in a token
+func validTokens(data []byte) bool {
+	for _, c := range data {
+		if c >= 'A' && c <= 'Z' {
+			continue
+		}
+		if c >= 'a' && c <= 'z' {
+			continue
+		}
+		if c >= '0' && c <= '9' {
+			continue
+		}
+		if bytes.ContainsAny(tokenChars, string(c)) {
+			continue
+		}
+		return false
+	}
+	return true
+}
---
+++ internal/headers/headers_test.go
@@ -0,0 +1,75 @@
+package headers
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHeaders(t *testing.T) {
+	// Test: Valid single header
+	headers := NewHeaders()
+	data := []byte("Host: localhost:42069\r\n\r\n")
+	n, done, err := headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["host"])
+	assert.Equal(t, 23, n)
+	assert.False(t, done)
+
+	// Test: Valid single spacing header
+	headers = NewHeaders()
+	data = []byte("      Host: localhost:42069\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["host"])
+	assert.Equal(t, 29, n)
+	assert.False(t, done)
+
+	// Test: Valid single spacing header
+	headers = NewHeaders()
+	data = []byte("      Host: localhost:42069\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["host"])
+	assert.Equal(t, 29, n)
+	assert.False(t, done)
+
+	// Test: Valid 2 headers with existing headers
+	headers = map[string]string{"host": "localhost:42069"}
+	data = []byte("User-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["host"])
+	assert.Equal(t, "curl/7.81.0", headers["user-agent"])
+	assert.Equal(t, 25, n)
+	assert.False(t, done)
+
+	// Test: Valid with special key
+	headers = NewHeaders()
+	data = []byte("H$st: localhost:42069\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	assert.Equal(t, "localhost:42069", headers["h$st"])
+	assert.Equal(t, 23, n)
+	assert.False(t, done)
+
+	// Test: Invalid spacing header
+	headers = NewHeaders()
+	data = []byte("       Host : localhost:42069       \r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+
+	// Test: Invalid key
+	headers = NewHeaders()
+	data = []byte("H©st: localhost:42069\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+}
