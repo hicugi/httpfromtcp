7b8ec164959babc5e1db0496eeb3a394d0322e29
Mag <hicugi@gmail.com>
2025-04-21 15:34:38 +0500

    feat: add handler in main httpserver cmd

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	"log"
+	"io"
 	"os"
 	"os/signal"
 	"syscall"
+	"myhttpfromtcp/internal/request"
+	"myhttpfromtcp/internal/response"
 	"myhttpfromtcp/internal/server"
 )
 
 const PORT = 42069
 
 func main() {
-	server, err := server.Serve(PORT)
+	server, err := server.Serve(PORT, handler)
 	if err != nil {
 		log.Fatalf("Error starting server: %v", err)
 	}
@@ -24,4 +27,23 @@ func main() {
 	<-sigChan
 
 	log.Println("Server gracefully stopped")
+}
+
+func handler(w io.Writer, req *request.Request) *server.HandlerError {
+	if req.RequestLine.RequestTarget == "/yourproblem" {
+		return &server.HandlerError{
+			StatusCode: response.STATUS_CODE_BAD_REQUEST,
+			Message:    "Your problem is not my problem\n",
+		}
+	}
+
+	if req.RequestLine.RequestTarget == "/myproblem" {
+		return &server.HandlerError{
+			StatusCode: response.STATUS_CODE_INTERNAL_ERROR,
+			Message:    "Woopsie, my bad\n",
+		}
+	}
+
+	w.Write([]byte("All good, frfr\n"))
+	return nil
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -4,18 +4,36 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"bytes"
+	"io"
 	"sync/atomic"
+	"myhttpfromtcp/internal/request"
 	"myhttpfromtcp/internal/response"
 )
 
 const CONNECTION_TYPE = "tcp"
 
+type Handler func(w io.Writer, req *request.Request) *HandlerError
+type HandlerError struct {
+	StatusCode response.StatusCode
+	Message    string
+}
+
+func (he HandlerError) Write(w io.Writer) {
+	response.WriteStatusLine(w, he.StatusCode)
+	messageBytes := []byte(he.Message)
+	headers := response.GetDefaultHeaders(len(messageBytes))
+	response.WriteHeaders(w, headers)
+	w.Write(messageBytes)
+}
+
 type Server struct {
 	listener net.Listener
+	handler  Handler
 	closed   atomic.Bool
 }
 
-func Serve(port int) (*Server, error) {
+func Serve(port int, handler Handler) (*Server, error) {
 	listener, err := net.Listen(CONNECTION_TYPE, fmt.Sprintf(":%d", port))
 	if err != nil {
 		log.Fatalf("Coun't create listener '%s' by port :%d\n", CONNECTION_TYPE, port)
@@ -24,6 +42,7 @@ func Serve(port int) (*Server, error) {
 
 	s := &Server {
 		listener: listener,
+		handler:  handler,
 	}
 
 	go s.listen()
@@ -57,10 +76,28 @@ func (s *Server) listen() {
 func (s *Server) handle(conn net.Conn) {
 	defer conn.Close()
 
-	response.WriteStatusLine(conn, response.STATUS_CODE_OK)
-	headers := response.GetDefaultHeaders(0)
-
-	if err := response.WriteHeaders(conn, headers); err != nil {
-		fmt.Printf("error: %v\n", err)
+	req, err := request.RequestFromReader(conn)
+	if err != nil {
+		hErr := &HandlerError{
+			StatusCode: response.STATUS_CODE_BAD_REQUEST,
+			Message:    err.Error(),
+		}
+		hErr.Write(conn)
+		return
 	}
+
+	buf := bytes.NewBuffer([]byte{})
+	hErr := s.handler(buf, req)
+	if hErr != nil {
+		hErr.Write(conn)
+		return
+	}
+
+	b := buf.Bytes()
+
+	response.WriteStatusLine(conn, response.STATUS_CODE_OK)
+	headers := response.GetDefaultHeaders(len(b))
+	response.WriteHeaders(conn, headers)
+
+	conn.Write(b)
 }
